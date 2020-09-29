3332e45cb34b2c42d64db6864a77e65d0e1f1d6b
Frederick Ernest <frederick.ernest@gmail.com>
2020-09-29 11:58:54 -0700

    change to lightning bolt icon with font awesome package

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,18 +4,12 @@ import React from 'react'
 const Header = props => (
   <header id="header" style={props.timeout ? { display: 'none' } : {}}>
     <div className="logo">
-      <span className="icon fa-diamond"></span>
+      <span className="icon fa-bolt"></span>
     </div>
     <div className="content">
       <div className="inner">
-        <h1>Dimension</h1>
-        <p>
-          A fully responsive site template designed by{' '}
-          <a href="https://html5up.net">HTML5 UP</a> and released
-          <br />
-          for free under the{' '}
-          <a href="https://html5up.net/license">Creative Commons</a> license.
-        </p>
+        <h1>LOCAL WEATHER</h1>
+        // WEATHER API VISUAL
       </div>
     </div>
     <nav>
