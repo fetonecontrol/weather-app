d5161f003f29a4759d58a24ed35939c9992d2331
Frederick Ernest <frederick.ernest@gmail.com>
2020-09-29 11:40:23 -0700

    add background image
    Co-authored-by: Kevin Davis <thekidnamedkd@gmail.com>

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 
 const Footer = (props) => (
     <footer id="footer" style={props.timeout ? {display: 'none'} : {}}>
-        <p className="copyright">&copy; Gatsby Starter - Dimension. Design: <a href="https://html5up.net">HTML5 UP</a>. Built with: <a href="https://www.gatsbyjs.org/">Gatsby.js</a></p>
+        <p className="copyright">&copy; Frederick Ernest | Kevin Davis | Thomas Glenn - Fall 2020 Epicodus - Built with: <a href="https://www.gatsbyjs.org/">Gatsby.js</a></p>
     </footer>
 )
 
@@ -12,3 +12,5 @@ Footer.propTypes = {
 }
 
 export default Footer
+
+//<p className="copyright">&copy; Gatsby Starter - Dimension. Design: <a href="https://html5up.net">HTML5 UP</a>. Built with: <a href="https://www.gatsbyjs.org/">Gatsby.js</a></p>
